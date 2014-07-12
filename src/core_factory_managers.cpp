0f97a7e27a63f9fdc93c37950f5a74bd1fdd7983
Dima Litvinov <dimalit@yandex.ru>
2014-07-12 19:19:27 +0300

    cleared core_factory_managers.*

--- src/core_factory_managers.cpp
+++ src/core_factory_managers.cpp
@@ -13,28 +13,3 @@
 #include <vector>
 #include <cassert>
 
-std::vector<int> v;
-
-void OdeInstanceFactoryManager::add(OdeInstanceFactory* f){
-	std::string key = f->getDisplayName();
-	assert(instance_factories.find(key)==instance_factories.end());
-	instance_factories[key] = f;
-}
-
-void OdeInstanceFactoryManager::remove(OdeInstanceFactory* f){
-	assert(instance_factories.find(f->getDisplayName())!=instance_factories.end());
-
-	// TODO: remove related solvers!
-
-	// remove itself
-	instance_factories.erase(f->getDisplayName());
-}
-
-std::vector<std::string> OdeInstanceFactoryManager::getInstanceNames() const {
-	std::vector<std::string> v;
-	std::for_each(instance_factories.begin(), instance_factories.end(), [&](const name_to_inst_map::value_type& pr){v.push_back(pr.second->getDisplayName());});
-	return v;
-}
-OdeInstanceFactory* OdeInstanceFactoryManager::getFactory(const std::string& name){
-	return instance_factories.at(name);
-}
